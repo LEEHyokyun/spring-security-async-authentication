8109dfd22adc220ce9451996f24dfdedbb91dc50
leehyokyun <gyrbs22@gmail.com>
2025-12-16 16:43:45 +0900

    fetch logout

--- src/main/java/spring/security/async/authentication/fetch/controller/FetchController.java
+++ src/main/java/spring/security/async/authentication/fetch/controller/FetchController.java
@@ -1,29 +1,46 @@
 package spring.security.async.authentication.fetch.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.security.async.authentication.auth.users.domain.vo.UserInfomation;
 
 @RestController
+@RequestMapping("/fetch")
 public class FetchController {
-    @PostMapping("/fetch/login")
+    @PostMapping("/login")
     public String fetchLogin(){
         return "fetch/login";
     }
 
-    @GetMapping("/fetch/user")
+    @PostMapping("/logout")
+    public String fetchLogout(HttpServletRequest request, HttpServletResponse response){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/user")
     public UserInfomation fetchUser(@AuthenticationPrincipal UserInfomation userInfomation){
         return userInfomation;
     }
 
-    @GetMapping("/fetch/manager")
+    @GetMapping("/manager")
     public UserInfomation fetchManager(@AuthenticationPrincipal UserInfomation userInfomation){
         return userInfomation;
     }
 
-    @GetMapping("/fetch/admin")
+    @GetMapping("/admin")
     public UserInfomation fetchAdmin(@AuthenticationPrincipal UserInfomation userInfomation){
         return userInfomation;
     }
